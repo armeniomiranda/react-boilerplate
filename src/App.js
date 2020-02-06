91b386742b25b00de20c3c9910c47fcbbe016c07
armeniomiranda <armeniomiranda@icloud.com>
2020-02-06 15:47:53 +0000

    Asynchronously load webpack bundles through code splitting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 
+const Warning = React.lazy(() => import('./Warning'));
 class App extends React.Component {
   state = {
     count: 0
@@ -13,6 +14,11 @@ class App extends React.Component {
         <h2>Count: {this.state.count}</h2>
         <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
         <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+        {this.state.count > 10 ? 
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+          : null}
       </div>
     )
   }
