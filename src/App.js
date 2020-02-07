6bcbe9a8a40a8b124a4099090efd3cc6695cfc84
armeniomiranda <armeniomiranda@icloud.com>
2020-02-07 14:28:42 +0000

    Create an error boundary

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import('./Warning'));
 class App extends React.Component {
-  state = {
-    count: 0
-  };
-
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
-        <h2>Count: {this.state.count}</h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
-        {this.state.count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
+        <h1>Hello World.</h1>
       </div>
     );
   }
