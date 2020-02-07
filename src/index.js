bb04f0d0416757f2c1f211ba6a1927eb120906ce
armeniomiranda <armeniomiranda@icloud.com>
2020-02-07 14:35:50 +0000

    Add and configure react-axe

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import DefaultErrorBoundary from './DefaultErrorBoundary';
 import './styles.css';
 
-// if (process.env.NODE_ENV === 'development') {
-//   const axe = require('react-axe')
-//   axe(React, ReactDOM, 1000)
-// }
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.render(
   <React.StrictMode>
